fe18f83ddb35c9546d86781e7a45a088e693d314
thZero.com <development@thzero.com>
2018-09-15 12:01:30 -0500

    master

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("thZero.NetCore.Library.Services.CircuitBreaker.Steeltoe")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a414c516-1af0-415f-8411-2003d0937547")]
